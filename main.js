7e4d301b26d313c276ee1c813b33a4e3c98cd5dd
AnaCarla2 <anacarlamartinezcaraballo@gmail.com>
2025-06-05 12:24:02 -0500

    Cambio de puerto

--- main.js
+++ main.js
@@ -12,8 +12,8 @@ const cardsPerPage = 8;
 
 
 Promise.all([
-  fetch('http://localhost:8081/servicios').then(res => res.json()),
-  fetch('http://localhost:8081/productos').then(res => res.json())
+  fetch('http://localhost:8080/servicios').then(res => res.json()),
+  fetch('http://localhost:8080/productos').then(res => res.json())
 ]).then(([services, products]) => {
   dataFetched = { services, products };
   renderCards(dataFetched.services);
